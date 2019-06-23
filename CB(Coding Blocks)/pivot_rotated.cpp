f6a8ec85e4f26355b192349d06d11801bd87e600
ayush <abnsl0014@gmail.com>
2019-06-23 17:37:49 +0530

    added

---
+++ CB(Coding Blocks)/pivot_rotated.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long int
+#define fr for(ll i=0;i<n;++i)
+#define in(a,n) ll a[n];for(ll i=0;i<n;i++)cin>>a[i];
+#define print(a,n) for(ll i=0;i<n;i++)cout<<a[i]<<' ';br
+#define printv(v) vector<ll> :: iterator it;for(it=v.begin();it!=v.end();it++)cout<<*it<<' ';br
+#define print2(a,b) cout<<a<<' '<<b;br
+#define print3(a,b,c) cout<<a<<' '<<b<<' '<<c;br
+#define sum(a,n) ll sum=0;for(ll i=0;i<n;i++)sum+=a[i];
+#define br cout<<endl;
+#define SPEED ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ff first
+#define ss second
+#define mod 1000000007
+using namespace std;
+int main(){
+    SPEED
+    ll n,m,x,ans,mid;
+    cin>>n;
+    in(a,n)
+    ll s=0;
+    ll e=n-1;
+    while(s<=e){
+         mid=(s+e)/2;
+         if(a[mid]<a[mid-1]){
+             break;
+         }
+        if(a[mid]<a[e]) {
+            //ans=mid;
+            e=mid;
+        }
+        else{
+            s=mid+1;
+        }
+    }
+    cout<<mid-1<<endl;
+    return 0;
+}
