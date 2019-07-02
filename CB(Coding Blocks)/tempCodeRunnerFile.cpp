a00072752428b7b33d194d59dcd619bd5932f973
ayush <abnsl0014@gmail.com>
2019-07-02 15:15:54 +0530

    added code

---
+++ CB(Coding Blocks)/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+while(!s.empty()) {
+    //         x=s.top()
+    //         s.pop();
+    //      if(s.empty()) {
+    //         area=(x*i);
+    //         ans=max(area,ans);        
+    //         }
+    //     else {
+    //         area=(x)*(i-s.top()-1);
+    //         ans=max(area,ans);
+    //     }
+
+    // }
